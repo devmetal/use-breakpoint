8fa0c5b09639b1d77ead85e204bc01e310113826
Metál Ádám <devmetal91@gmail.com>
2019-12-10 11:16:04 +0100

    Update useResize.ts

--- src/useResize.ts
+++ src/useResize.ts
@@ -1,13 +1,47 @@
 import { useEffect } from 'react'
 
+let windowResizeEventAdded = false;
+
+const callbacks: Array<Function> = [];
+
+const handleWindowResizeEvent = () => callbacks.forEach((cb) => cb());
+
+const removeWindowResizeEvent = () =>
+    window.removeEventListener('resize', handleWindowResizeEvent);
+
+const initWindowResizeEvent = () =>
+    window.addEventListener('resize', handleWindowResizeEvent);
+
+const onWindowResize = (callback) => {
+    if (!windowResizeEventAdded) {
+        initWindowResizeEvent();
+        windowResizeEventAdded = true;
+    }
+
+    callbacks.push(callback);
+
+    return () => {
+        const i = callbacks.findIndex(callback);
+        if (i !== -1) {
+            callbacks.splice(i, 1);
+        }
+
+        if (!callbacks.length) {
+            removeWindowResizeEvent();
+            windowResizeEventAdded = false;    
+        }
+    }
+}
+
 export default function useResize(callback, throttle = 50) {
     useEffect(() => {
         let timeout
+        
         const handleResize = () => {
             clearTimeout(timeout)
             timeout = setTimeout(callback, throttle)
         }
-        window.addEventListener('resize', handleResize)
-        return () => window.removeEventListener('resize', handleResize)
+
+        return onWindowResize(handleResize);
     }, [])
 }
